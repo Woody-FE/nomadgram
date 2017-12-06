f6de76f7fe75843e1d5c4d8a01a79a878f230fd2
Taein <kti91@likelion.org>
2017-12-06 13:58:30 +0900

    backend finish!

--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -7,7 +7,7 @@ from nomadgram.users import models as user_models
 from nomadgram.users import serializers as user_serializers
 
 
-class Feed(APIView):
+class Images(APIView):
     
     def get(self, request, format=None):
 
@@ -36,6 +36,22 @@ class Feed(APIView):
         serializer = serializers.ImageSerializer(sorted_list, many=True)
 
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        
+        user = request.user
+        
+        serializer = serializers.InputImageSerializer(data=request.data)
+
+        if serializer.is_valid():
+            
+            serializer.save(creator=user)
+
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+        else:
+            
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class LikeImage(APIView):
     
